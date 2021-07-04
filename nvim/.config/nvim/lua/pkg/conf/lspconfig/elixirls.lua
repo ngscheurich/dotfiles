86332922ec49300088f5490b43fa9f904a57663b
Nicholas Scheurich <nick@scheurich.me>
2021-07-04 03:02:12 -0500

    Simplify ElixirLS config

--- nvim/.config/nvim/lua/pkg/conf/lspconfig/elixirls.lua
+++ nvim/.config/nvim/lua/pkg/conf/lspconfig/elixirls.lua
@@ -1,30 +1,7 @@
 return function(on_attach)
-  local lspconfig = require("lspconfig")
-
-  local job = vim.loop.new_async(vim.schedule_wrap(function()
-    local lscdir = "/usr/local/opt/elixir-ls/release"
-    local stdout = vim.fn.system("elixir -v")
-    local otpver = string.match(stdout, "OTP (%d+)")
-
-    lspconfig.elixirls.setup({
-      cmd = {lscdir .. "/1.11.2-otp-" .. otpver .. "/language_server.sh"},
-      on_attach = on_attach,
-      settings = {elixirLS = {fetchDeps = false}},
-    })
-
-    local ft = vim.bo.filetype
-    if ft == "elixir" or ft == "eelixir" then
-      lspconfig.elixirls.manager.try_add()
-    end
-  end))
-
-  job:send()
+  require("lspconfig").elixirls.setup({
+    cmd = {"/usr/local/opt/elixir-ls/language_server.sh"},
+    on_attach =  on_attach,
+    settings = {elixirLS = {fetchDeps = false}},
+  })
 end
-
--- return function(on_attach)
---   require("lspconfig").elixirls.setup({
---     cmd = "/usr/local/opt/elixir-ls/language_server.sh",
---     on_attach =  on_attach,
---     settings = {elixirLS = {fetchDeps = false}},
---   })
--- end
