35867a9be25b4b8640603fa70a480170338fd2ec
Nicholas Scheurich <nick@scheurich.me>
2021-07-14 23:46:22 -0500

    Formatting

--- nvim/.config/nvim/lua/pkg/conf/lspconfig/sumneko_lua.lua
+++ nvim/.config/nvim/lua/pkg/conf/lspconfig/sumneko_lua.lua
@@ -1,13 +1,31 @@
 return function(on_attach)
-    local lspstatus = require('lsp-status')
-    local lua_root = "/usr/local/opt/lua-language-server"
-    local lua_bin = lua_root .. "/bin/macOS/lua-language-server"
-    local lua_main = lua_root .. "/main.lua"
+  local lua_root = "/usr/local/opt/lua-language-server"
+  local lua_bin = lua_root .. "/bin/macOS/lua-language-server"
+  local lua_main = lua_root .. "/main.lua"
 
-    require("lspconfig").sumneko_lua.setup({
-        cmd = {lua_bin, "-E", lua_main},
-        on_attach = on_attach,
-        capabilities = lspstatus.capabilities,
-        settings = {Lua = {diagnostics = {enable = true, globals = {"vim"}}}}
-    })
+  local runtime_path = vim.split(package.path, ';')
+  table.insert(runtime_path, "lua/?.lua")
+  table.insert(runtime_path, "lua/?/init.lua")
+
+  require("lspconfig").sumneko_lua.setup({
+    cmd = {lua_bin, "-E", lua_main},
+    on_attach = on_attach,
+    settings = {
+      Lua = {
+        runtime = {
+          version = "LuaJIT",
+          path = runtime_path,
+        },
+        diagnostics = {
+          globals = {"vim"}
+        },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  })
 end
