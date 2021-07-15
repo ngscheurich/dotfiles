0d85ba0749213b9d1093195c8acba5e180be442c
Nicholas Scheurich <nick@scheurich.me>
2021-07-14 23:46:13 -0500

    Formatting

--- nvim/.config/nvim/lua/pkg/conf/lspconfig/omnisharp.lua
+++ nvim/.config/nvim/lua/pkg/conf/lspconfig/omnisharp.lua
@@ -1,9 +1,9 @@
 return function(on_attach)
-    local pid = vim.fn.getpid()
-    local omnisharp_bin = "/Users/nick/tools/omnisharp-osx/run"
+  local pid = vim.fn.getpid()
+  local omnisharp_bin = "/Users/nick/tools/omnisharp-osx/run"
 
-    require("lspconfig").omnisharp.setup({
-        cmd = {omnisharp_bin, "--languageserver", "--hostPID", tostring(pid)},
-        on_attach = on_attach
-    })
+  require("lspconfig").omnisharp.setup({
+    cmd = {omnisharp_bin, "--languageserver", "--hostPID", tostring(pid)},
+    on_attach = on_attach
+  })
 end
