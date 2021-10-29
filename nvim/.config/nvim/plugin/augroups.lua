3e030256010075256f766396a2610a31d5e21a9a
Nicholas Scheurich <nick@scheurich.me>
2021-10-29 11:49:57 -0500

    Stop LSP client/Elixir LS spamming messages list

--- nvim/.config/nvim/plugin/augroups.lua
+++ nvim/.config/nvim/plugin/augroups.lua
@@ -8,3 +8,8 @@ local vim = ngs.vim
 vim.augroup("yank", function()
   vim.autocmd("TextYankPost", "*", "lua vim.highlight.on_yank()")
 end)
+
+-- TODO:: Why the fuck is the Elixir LS dumping every message ever sometimes?
+vim.augroup("suppress-more", function ()
+  vim.autocmd("BufEnter", "*.ex,*.exs", "set nomore")
+end)
