69eb4080654b37f4c0c0bf10b70b9e3b2954091e
Nicholas Scheurich <nick@Nicholass-MacBook-Pro.local>
2021-12-04 21:15:05 -0600

    WIP Nix

---
+++ nixpkgs/.config/nixpkgs/darwin.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+# List packages installed in system profile.
+  environment.systemPackages = [];
+
+# Use a custom configuration.nix location.
+  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin.nix";
+
+# Auto upgrade nix package and the daemon service.
+  nix.package = pkgs.nix;
+  services.nix-daemon.enable = true;
+
+# Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;
+
+# Used for backwards compatibility. Read the changelog before changing.
+  system.stateVersion = 4;
+
+# Configure the dock.
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.mineffect = "scale";
+  system.defaults.dock.tilesize = 48;
+}
---
+++ nixpkgs/.config/nixpkgs/home.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "nick";
+  home.homeDirectory = "/Users/nick";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    asciinema
+    asdf-vm
+    astyle
+    awscli
+    aws-vault
+    bat
+    cocoapods
+    ctags
+    curl
+    dasht
+    diff-so-fancy
+    direnv
+    elixir_ls
+    exa
+    fd
+    figlet
+    fish
+    fzf
+    fzy
+    gh
+    gist
+    git
+    glow
+    gnupg
+    htop
+    httpie
+    jq
+    lazygit
+    neofetch
+    neovim
+    ngrok
+    ranger
+    ripgrep
+    shellcheck
+    stripe-cli
+    stow
+    taskwarrior
+    taskwarrior-tui
+    terraform-ls
+    thefuck
+    tldr
+    tmux
+    tree
+  ];
+}
