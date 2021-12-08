06a07c0d18bdd35ab88b33fcb7f42e0376a3cf05
Nicholas Scheurich <nick@scheurich.me>
2021-12-08 01:16:58 -0600

    Use ngrok v1 (open source)

--- nixpkgs/.config/nixpkgs/home.nix
+++ nixpkgs/.config/nixpkgs/home.nix
@@ -1,25 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "nick";
   home.homeDirectory = "/Users/nick";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "22.05";
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  # nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
     asciinema
@@ -52,7 +38,7 @@
     lazygit
     neofetch
     neovim
-    ngrok
+    ngrok-1
     ranger
     ripgrep
     shellcheck
