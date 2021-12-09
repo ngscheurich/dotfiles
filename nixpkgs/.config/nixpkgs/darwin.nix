0fbdf21199a29b0fda1ee7119cd4c365cdf2b92a
Nicholas Scheurich <nick@scheurich.me>
2021-12-09 01:49:04 -0600

    Formatting

--- nixpkgs/.config/nixpkgs/darwin.nix
+++ nixpkgs/.config/nixpkgs/darwin.nix
@@ -1,23 +1,23 @@
 { config, pkgs, ... }:
 
 {
-# List packages installed in system profile.
+  # List packages installed in system profile.
   environment.systemPackages = [];
 
-# Use a custom configuration.nix location.
+  # Use a custom configuration.nix location.
   environment.darwinConfig = "$HOME/.config/nixpkgs/darwin.nix";
 
-# Auto upgrade nix package and the daemon service.
+  # Auto upgrade nix package and the daemon service.
   nix.package = pkgs.nix;
   services.nix-daemon.enable = true;
 
-# Create /etc/zshrc that loads the nix-darwin environment.
+  # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;
 
-# Used for backwards compatibility. Read the changelog before changing.
+  # Used for backwards compatibility. Read the changelog before changing.
   system.stateVersion = 4;
 
-# Configure the dock.
+  # Configure the dock.
   system.defaults.dock.autohide = true;
   system.defaults.dock.mineffect = "scale";
   system.defaults.dock.tilesize = 48;
