8d91265d9fa46f44d78800ba7824a9ca641f488e
Nicholas Scheurich <nick@scheurich.me>
2022-02-05 21:12:34 -0600

    Fix XDG directory name

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,7 +35,7 @@ set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/
 set -gx KERL_CONFIGURE_OPTIONS --without-javac
 set -gx NVIM_LISTEN_ADDRESS /tmp/nvimsocket
 set -gx RANGER_DEVICONS_SEPARATOR "  "
-set -gx XDG_CONFIG_DATA $HOME/.local/share
+set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CONFIG_HOME $HOME/.config
 
 fish_add_path $ANDROID_HOME/emulator
