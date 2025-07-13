6ac62759004d98c7fa293f4eaa0fcb25779f2618
N. G. Scheurich <nick@scheurich.me>
2025-07-13 18:32:39 -0500

    Initial commit

---
+++ dot_config/nvim/lsp/gleam.lua
@@ -0,0 +1,5 @@
+vim.lsp.config["gleam"] = {
+  cmd = { "gleam", "lsp" },
+  filetypes = { "gleam" },
+  root_markers = { "gleam.toml", ".git" },
+}
---
+++ dot_config/nvim/lsp/lexical.lua
@@ -0,0 +1,5 @@
+vim.lsp.config["lexical"] = {
+  cmd = { "lexical" },
+  filetypes = { "elixir", "heex", ".git" },
+  root_markers = { "mix.exs" },
+}
