fed0544357f24f1f5ae7e2b33cd452d67065224b
N. G. Scheurich <nick@scheurich.me>
2025-07-14 19:51:36 -0500

    WIP

--- init.sh
+++ init.sh
@@ -2,9 +2,11 @@
 
 # Initialize dotfiles
 
+LOCAL_BIN="$HOME/.local/bin"
+
 # Install chezmoi if necessary
 if ! command -v chezmoi > /dev/null 2>&1; then
-  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$LOCAL_BIN"
 fi
 
-chezmoi init --apply --verbose https://github.com/ngscheurich/dotfiles.git
+"$LOCAL_BIN/chezmoi" init --apply --verbose ngscheurich
