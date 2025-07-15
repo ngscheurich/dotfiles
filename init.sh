fd979c7687f6fd0f780e5614dd40f449c950d732
N. G. Scheurich <nick@scheurich.me>
2025-07-14 21:53:09 -0500

    WIP

--- init.sh
+++ init.sh
@@ -5,13 +5,12 @@ LOCAL_BIN="$HOME/.local/bin"
 msg() {
   local BOLD="\033[1m"
   local RESET="\033[0m"
-  echo "${BOLD}\033[$1m$2${RESET}"
+  echo "${BOLD}[dotfiles] \033[$1m$2${RESET}"
 }
-
 
 # Install chezmoi if necessary
 if [ ! -f "$LOCAL_BIN/chezmoi" ]; then
-  msg 33 "🏠 Installing chezmoi..."
+  msg 34 "🏠 Installing chezmoi..."
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$LOCAL_BIN"
 else
   msg 32 "🏠 Chezmoi detected ✓"
@@ -19,7 +18,7 @@ fi
 
 # Install Homebrew if necessary
 if [ ! -d /opt/homebrew ]; then
-  msg 33 "🍺 Installing Homebrew..."
+  msg 34 "🍺 Installing Homebrew..."
   /bin/bash -c \
     "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -27,10 +26,10 @@ else
   msg 32 "🍺 Homebrew detected ✓"
 fi
 
-# Install chezmoi template dependencies
-msg "🍻 Installing chezmoi template dependencies..."
-brew install 1password 1password-cli gum
+# Install shell script dependencies
+msg 34 "🐚 Installing script dependencies..."
+brew install gum
 
 # Initialize and apply chezmoi local state
-msg "🎒 Initializing dotfiles..."
+msg 34 "🎒 Initializing dotfiles..."
 "$LOCAL_BIN/chezmoi" init --apply ngscheurich
