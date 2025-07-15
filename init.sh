b7eb65d1902585efb7d74c24394a26e1ec2ee922
N. G. Scheurich <nick@scheurich.me>
2025-07-14 20:03:54 -0500

    WIP

--- init.sh
+++ init.sh
@@ -9,4 +9,4 @@ if ! command -v chezmoi > /dev/null 2>&1; then
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$LOCAL_BIN"
 fi
 
-"$LOCAL_BIN/chezmoi" init --apply --verbose ngscheurich
+"$LOCAL_BIN/chezmoi" init --apply ngscheurich
