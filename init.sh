515ad41eaa9b0cebf104aee8c05313a445106e74
N. G. Scheurich <nick@scheurich.me>
2025-07-19 16:06:32 -0500

    WIP

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -n "${DEBUG_SCRIPTS+x}" ] && [ "$DEBUG_SCRIPTS" -eq 1 ]; then
+if [ "${DEBUG_SCRIPTS:-0}" = "1" ]; then
   set -ev
 else
   set -e
@@ -9,54 +9,88 @@ fi
 LOCAL_BIN="$HOME/.local/bin"
 HOMEBREW_BIN="/opt/homebrew/bin"
 
-
 msg() {
-  local BOLD="\033[1m"
-  local RESET="\033[0m"
-  echo -e "${BOLD}[dotfiles] \033[$1m$2${RESET}"
+  printf "\033[1m[dotfiles] \033[%sm%s\033[0m\n" "$1" "$2"
 }
 
-msg 34 "🍺 Installing Homebrew..."
-exit 0
+brew() {
+  "$HOMEBREW_BIN/brew" "$@"
+}
 
+is_installed() {
+  brew_ls=$(/opt/homebrew/bin/brew list)
+  printf "%s" "$brew_ls" | grep -Fxq "$1"
+}
+
+# ----------------------------------------------------------
 # Install Homebrew
-if [ ! -d "$HOMEBREW_BIN" ]; then
-  msg 34 "🍺 Installing Homebrew..."
-  /bin/bash -c \
-    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-  msg 32 "🍺 Homebrew detected ✓"
-fi
+# ----------------------------------------------------------
+install_homebrew() {
+  if [ ! -d "$HOMEBREW_BIN" ]; then
+    msg 34 "🍺 Installing Homebrew..."
+    /bin/bash -c \
+      "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  else
+    msg 32 "🍺 Homebrew detected ✓"
+  fi
+}
 
-# Install run script dependencies
-if [ ! -f /opt/homebrew/bin/gum ] || [ ! -f /opt/homebrew/bin/op ]; then
-  msg 34 "🐚 Installing setup dependencies..."
-  /opt/homebrew/bin/brew install --cask 1password-cli
-  /opt/homebrew/bin/brew install gum
-else
-  msg 32 "🐚 Setup dependencies detected ✓"
-fi
+# ----------------------------------------------------------
+# Install setup dependencies
+# ----------------------------------------------------------
+install_dependencies() {
+  missing=0
+  if ! is_installed gum; then missing=1; fi
+  if ! is_installed 1password; then missing=1; fi
+  if ! is_installed 1password-cli; then missing=1; fi
 
-# Halt execution until user presses a key
-read -n 1 -rsp "Press any key to continue..."; echo
-# if [ "${SHELL##*/}" = "fish" ]; then
-#   read -l -P "Press any key to continue..."
-# else
-#   read -n 1 -rsp "Press any key to continue..."; echo
-# fi
+  if [ "$missing" -eq 1 ]; then
+    msg 34 "🐚 Installing setup dependencies..."
+    if ! is_installed gum; then brew install gum; fi
+    if ! is_installed 1password; then brew install --cask 1password; fi
+    if ! is_installed 1password-cli; then brew install --cask 1password-cli; fi
+  else
+    msg 32 "🐚 Setup dependencies detected ✓"
+  fi
+}
 
+# ----------------------------------------------------------
+# Ensure logged into 1Password
+# ----------------------------------------------------------
+get_1password_login() {
+}
+printf "Press Enter to continue..."
+read -r _x 
+
+# ----------------------------------------------------------
 # Install chezmoi
-if [ ! -f "$LOCAL_BIN/chezmoi" ]; then
-  msg 34 "🏠 Installing chezmoi..."
-  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$LOCAL_BIN"
-else
-  msg 32 "🏠 Chezmoi detected ✓"
-fi
+# ----------------------------------------------------------
+install_chezmoi() {
+  if [ ! -f "$LOCAL_BIN/chezmoi" ]; then
+    msg 34 "🏠 Installing chezmoi..."
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$LOCAL_BIN"
+  else
+    msg 32 "🏠 Chezmoi detected ✓"
+  fi
+}
 
+# ----------------------------------------------------------
 # Initialize and apply chezmoi state
-if [ ! -d "$HOME/.local/share/chezmoi" ]; then
-  msg 34 "🎒 Initializing dotfiles..."
-  "$LOCAL_BIN/chezmoi" init --apply ngscheurich
-else
-  msg 32 "🎒 Dotfiles detected ✓"
-fi
+# ----------------------------------------------------------
+initialize_chezmoi() {
+  if [ ! -d "$HOME/.local/share/chezmoi" ]; then
+    msg 34 "🎒 Initializing dotfiles..."
+    "$LOCAL_BIN/chezmoi" init --apply ngscheurich
+  else
+    msg 32 "🎒 Dotfiles detected ✓"
+  fi
+}
+
+main() {
+  install_homebrew 
+  install_dependencies
+  install_chezmoi 
+  initialize_chezmoi
+}
+
+main
