3d0dac9476ea04b93ebced0b1b5064d6a5d3cfd1
N. G. Scheurich <nick@scheurich.me>
2025-07-29 14:33:39 -0500

    feat: set up additional LSP servers

---
+++ home/dot_config/nvim/lsp/fish_lsp.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = { "fish-lsp", "start" },
+  filetypes = { "fish" },
+  root_markers = { "config.fish", ".git" },
+}
---
+++ home/dot_config/nvim/lsp/tombi.lua
@@ -0,0 +1,11 @@
+return {
+  cmd = { "tombi", "lsp" },
+  filetypes = { "toml" },
+  root_markers = {
+    "tombi.toml",
+    "pyproject.toml",
+    "Cargo.toml",
+    "Project.toml",
+    ".git",
+  },
+}
