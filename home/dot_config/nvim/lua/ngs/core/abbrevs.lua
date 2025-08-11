196ab24b2b6b4ca83ecc631b0adce2c0c3d87139
N. G. Scheurich <nick@scheurich.me>
2025-08-10 21:17:21 -0500

    feat(nvim): add Xa abbrev

--- home/dot_config/nvim/lua/ngs/core/abbrevs.lua
+++ home/dot_config/nvim/lua/ngs/core/abbrevs.lua
@@ -3,6 +3,7 @@ vim.cmd.cabbrev("Wa", "wa")
 vim.cmd.cabbrev("Q", "q")
 vim.cmd.cabbrev("Qa", "qa")
 vim.cmd.cabbrev("Qa!", "qa!")
+vim.cmd.cabbrev("Xa", "xa")
 vim.cmd.cabbrev("cc", "CodeCompanion")
 
 vim.cmd.iabbrev("Gridpoint", "GridPoint")
