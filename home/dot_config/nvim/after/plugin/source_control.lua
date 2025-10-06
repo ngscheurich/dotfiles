6cb2b641868b45f615140743d03ac408ad4e0bba
N. G. Scheurich <nick@scheurich.me>
2025-10-05 22:19:10 -0500

    feat: update deprecated GitSigns functions

--- home/dot_config/nvim/after/plugin/source_control.lua
+++ home/dot_config/nvim/after/plugin/source_control.lua
@@ -12,13 +12,24 @@ if gitsigns_ok then
   })
 
   vim.keymap.set("n", "<Leader>gb", gitsigns.toggle_current_line_blame, { desc = "Line blame (toggle)" })
-  -- TODO Deprecated
-  vim.keymap.set("n", "<Leader>gd", gitsigns.toggle_deleted, { desc = "Deleted (toggle)" })
+  vim.keymap.set("n", "<Leader>gd", gitsigns.preview_hunk_inline, { desc = "Deleted (toggle)" })
   vim.keymap.set("n", "<Leader>gh", gitsigns.toggle_linehl, { desc = "Line highlight (toggle)" })
   vim.keymap.set("n", "<Leader>gp", gitsigns.preview_hunk, { desc = "Preview hunk" })
   vim.keymap.set("n", "<Leader>gr", gitsigns.reset_hunk, { desc = "Reset hunk" })
-  -- TODO Deprecated
-  vim.keymap.set("n", "]h", gitsigns.next_hunk, { desc = "Next hunk" })
-  -- TODO Deprecated
-  vim.keymap.set("n", "[h", gitsigns.prev_hunk, { desc = "Previous hunk" })
+
+  vim.keymap.set("n", "[h", function()
+    gitsigns.nav_hunk("prev")
+  end, { desc = "Previous hunk" })
+
+  vim.keymap.set("n", "]h", function()
+    gitsigns.nav_hunk("next")
+  end, { desc = "Next hunk" })
+
+  vim.keymap.set("n", "[H", function()
+    gitsigns.nav_hunk("first")
+  end, { desc = "First hunk" })
+
+  vim.keymap.set("n", "]H", function()
+    gitsigns.nav_hunk("last")
+  end, { desc = "Last hunk" })
 end
