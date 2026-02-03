0541a02afcfdfe4cf07f38ded463d06682738e2e
N. G. Scheurich <nick@scheurich.me>
2026-02-02 20:57:08 -0600

    fix: keybind collisions

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -13,3 +13,9 @@ fzf --fish | source
 atuin init fish --disable-up-arrow | source
 zoxide init fish | source
 starship init fish | source
+
+# Customize fzf binds
+bind --erase --mode insert ctrl-t
+bind --erase --mode insert ctrl-r
+bind --mode insert alt-t fzf-file-widget
+bind --mode insert alt-r fzf-history-widget
