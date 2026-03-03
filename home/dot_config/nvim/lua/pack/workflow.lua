86213283e38a929a3ff1752493abb3c84cb4e1b4
N. G. Scheurich <nick@scheurich.me>
2026-03-03 14:18:23 -0600

    fix(nvim): quicker.nvim setup

--- home/dot_config/nvim/lua/pack/workflow.lua
+++ home/dot_config/nvim/lua/pack/workflow.lua
@@ -1,6 +1,11 @@
 local add = vim.pack.add
 local now_if_args, on_filetype = Config.now_if_args, Config.on_filetype
 
+-- ┎──────────────────────────────────────────────────┬────────────────────────┐
+-- ┃  oil.nvim                                       │  workflow             │
+-- ┠──────────────────────────────────────────────────┴────────────────────────┤
+-- ┃   Explore the filesystem and edit it like a buffer                       │
+-- ┖───────────────────────────────────────────────────────────────────────────┘
 now_if_args(function()
   add({ "https://github.com/stevearc/oil.nvim" })
 
@@ -30,7 +35,12 @@ now_if_args(function()
   vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" })
 end)
 
-on_filetype(
-  "qf",
-  function() add({ "https://github.com/stevearc/quicker.nvim" }) end
-)
+-- ┎──────────────────────────────────────────────────┬────────────────────────┐
+-- ┃  quicker.nvim                                   │  workflow, ui         │
+-- ┠──────────────────────────────────────────────────┴────────────────────────┤
+-- ┃  Improved UI and workflow for the quickfix list                          │
+-- ┖───────────────────────────────────────────────────────────────────────────┘
+on_filetype("qf", function()
+  add({ "https://github.com/stevearc/quicker.nvim" })
+  require("quicker").setup()
+end)
