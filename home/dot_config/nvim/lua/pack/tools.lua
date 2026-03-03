4d1b23818272abdf0fa8e994b1a00937ef8ec7be
N. G. Scheurich <nick@scheurich.me>
2026-03-02 21:37:31 -0600

    feat(nvim): add Minty

--- home/dot_config/nvim/lua/pack/tools.lua
+++ home/dot_config/nvim/lua/pack/tools.lua
@@ -1,5 +1,20 @@
-Config.on_filetype("sql", function()
-  vim.pack.add({
+local add = vim.pack.add
+local later, on_filetype = Config.later, Config.on_filetype
+
+-- ┎──────────────────────────────────────────────────┬────────────────────────┐
+-- ┃  Minty                                          │  tools                │
+-- ┠──────────────────────────────────────────────────┴────────────────────────┤
+-- ┃  Preview and manipulate colors                                           │
+-- ┖───────────────────────────────────────────────────────────────────────────┘
+later(function() add({ "https://github.com/nvzone/minty" }) end)
+
+-- ┎──────────────────────────────────────────────────┬────────────────────────┐
+-- ┃  vim-dadbod-ui                                  │  tools                │
+-- ┠──────────────────────────────────────────────────┴────────────────────────┤
+-- ┃  Query relational databases with dadbod.vim                              │
+-- ┖───────────────────────────────────────────────────────────────────────────┘
+on_filetype("sql", function()
+  add({
     "tpope/vim-dadbod",
     "kristijanhusak/vim-dadbod-ui",
     "kristijanhusak/vim-dadbod-completion",
@@ -7,3 +22,10 @@ Config.on_filetype("sql", function()
 
   vim.g.db_ui_use_nerd_fonts = 1
 end)
+
+-- ┎──────────────────────────────────────────────────┬────────────────────────┐
+-- ┃  Volt                                           │  tools                │
+-- ┠──────────────────────────────────────────────────┴────────────────────────┤
+-- ┃  Fast and reactive UI framework                                          │
+-- ┖───────────────────────────────────────────────────────────────────────────┘
+later(function() add({ "https://github.com/nvzone/volt" }) end)
