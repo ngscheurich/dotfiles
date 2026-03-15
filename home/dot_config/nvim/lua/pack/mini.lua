291b5bfe80f163f489bd832fcf6d8dc2d350d0df
N. G. Scheurich <nick@scheurich.me>
2026-03-15 18:51:40 -0500

    conf(nvim): adjust mini hipatterns

--- home/dot_config/nvim/lua/pack/mini.lua
+++ home/dot_config/nvim/lua/pack/mini.lua
@@ -26,10 +26,10 @@ later(function()
   local hi_words = extra.gen_highlighter.words
   hipatterns.setup({
     highlighters = {
-      fixme = hi_words({ "FIXME", "Fixme", "fixme" }, "MiniHipatternsFixme"),
-      hack = hi_words({ "HACK", "Hack", "hack" }, "MiniHipatternsHack"),
-      todo = hi_words({ "TODO", "Todo", "todo" }, "MiniHipatternsTodo"),
-      note = hi_words({ "NOTE", "Note", "note" }, "MiniHipatternsNote"),
+      fixme = hi_words({ "FIXME" }, "MiniHipatternsFixme"),
+      hack = hi_words({ "HACK" }, "MiniHipatternsHack"),
+      todo = hi_words({ "TODO" }, "MiniHipatternsTodo"),
+      note = hi_words({ "NOTE" }, "MiniHipatternsNote"),
 
       hex_color = hipatterns.gen_highlighter.hex_color(),
     },
