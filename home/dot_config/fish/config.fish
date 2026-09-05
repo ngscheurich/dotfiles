a0e0182be9786363ddff1cf81f014f682333c437
N. G. Scheurich <nick@scheurich.me>
2026-09-05 15:57:40 -0500

    feat(fish): remove theme

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -3,11 +3,6 @@ status is-interactive; or exit
 # Set command line key bindings
 set -g fish_key_bindings fish_hybrid_key_bindings
 
-# Load theme
-if test -e "$HOME/.theme/shell.fish"
-    source "$HOME/.theme/shell.fish"
-end
-
 # Initialize shell tools
 fzf --fish | source
 atuin init fish --disable-up-arrow | source
