14b6953b607933ad425ed0e3bd4cae58d577c69e
N. G. Scheurich <nick@scheurich.me>
2026-09-13 21:23:11 -0500

    conf(fish): source script-installed mise

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -6,9 +6,9 @@ set -g fish_key_bindings fish_hybrid_key_bindings
 # Initialize shell tools
 atuin init fish --disable-up-arrow | source
 fzf --fish | source
-mise activate fish | source
 starship init fish | source
 zoxide init fish | source
+~/.local/bin/mise activate fish | source
 
 # Customize key binds
 bind --erase --mode insert ctrl-f
