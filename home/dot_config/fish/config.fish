fab796ecf708a8067e276711531c835733ce0dc4
N. G. Scheurich <nick@scheurich.me>
2026-09-13 21:49:08 -0500

    fix(fish): re-establish ctrl-r -> atuin search -i

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -4,14 +4,12 @@ status is-interactive; or exit
 set -g fish_key_bindings fish_hybrid_key_bindings
 
 # Initialize shell tools
-atuin init fish --disable-up-arrow | source
 fzf --fish | source
+atuin init fish --disable-up-arrow | source
 starship init fish | source
 zoxide init fish | source
 ~/.local/bin/mise activate fish | source
 
-# Customize key binds
-bind --erase --mode insert ctrl-f
-bind --erase --mode insert ctrl-b
+# Customize fzf binds
 bind --erase --mode insert ctrl-t
 bind --mode insert ctrl-f fzf-file-widget
