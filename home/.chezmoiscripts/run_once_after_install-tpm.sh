237975768215e6a4eeda832b8375d0a7203b478c
N. G. Scheurich <nick@scheurich.me>
2026-09-14 19:01:55 -0500

    feat(scripts): update scripts

--- home/.chezmoiscripts/run_once_after_install-tpm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TPMDIR="$XDG_DATA_HOME/tpm/plugins/tpm"
-
-if [ ! -d "$TPMDIR" ]; then
-  git clone https://github.com/tmux-plugins/tpm "$TPMDIR"
-fi
--- home/.chezmoiscripts/run_once_after_install-uv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -LsSf https://astral.sh/uv/install.sh | sh
