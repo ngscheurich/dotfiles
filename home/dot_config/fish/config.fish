c46f8eba1b1eeeffb9b1d0c8177683e60342dd52
N. G. Scheurich <nick@scheurich.me>
2026-09-23 10:16:38 -0500

    feat(fish): use blinky cursor

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -3,6 +3,12 @@ status is-interactive; or exit
 # Set command line key bindings
 set -g fish_key_bindings fish_hybrid_key_bindings
 
+# Use blinky cursor
+set -g fish_cursor_default block blink
+set -g fish_cursor_insert line blink
+set -g fish_cursor_replace_one underscore blink
+set -g fish_cursor_visual block blink
+
 # Initialize shell tools
 fzf --fish | source
 atuin init fish --disable-up-arrow | source
